ccb784937fd0d01d1ba4820394219256da96be6c
Bartłomiej Strózik <bartek.strozik@gmail.com>
2021-11-15 23:50:11 +0100

    finished tasks: 5,7,8

--- lab3/zadanie07/script.js
+++ lab3/zadanie07/script.js
@@ -1,3 +1,20 @@
+function compareDentensities (city_a, city_b) {
+    return city_b.dentensity - city_a.dentensity;
+}
+
+function compareNames (city_a, city_b) {
+    if (city_a.name > city_b.name) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+function compareAreas (city_a, city_b) {
+    return city_b.area - city_a.area;
+}
+
 const renderCities = async() => {
     let uri = "http://localhost:3000/cities";
     const res = await fetch(uri);
@@ -11,7 +28,7 @@ const renderCities = async() => {
         .join(", ");
 
 
-    /* task (b) */
+    /* task (b) give me cities containing two characters 'a' */
     template = "";
     cities.forEach(city => {
         if(city.name.indexOf('a') > -1){
@@ -26,24 +43,74 @@ const renderCities = async() => {
     const two_a_chars_cities = document.querySelector(".two-a-chars-cities");
     two_a_chars_cities.innerHTML = template;
     
-    /* task (c) */
-    template = "";
-    let max_dentensity = 0;
-    cities.forEach(city => {
-        if(city.dentensity > max_dentensity){
-            template = `${city.name}: ${city.dentensity}`;
-        }
-    });
-    const max_dentensity_city = document.querySelector(".max-dentensity");
-    max_dentensity_city.innerHTML = template;
+    /* task (b) version 2*/
+    template = cities
+        .filter(city => city.name.indexOf('a') > -1)
+        .map(city => city.name)
+        .join(", ");
+    //console.log(template);
+
+    /* task (c) give me fith by dentensity property city */
+    cities.sort(compareDentensities);
+    let fith_dentensity_city = cities[4];
+    cities.sort(compareNames);
+    template = `${fith_dentensity_city.name}: ${fith_dentensity_city.dentensity}`;
+
+    const fifth_dentensity_city = document.querySelector(".fifth-dentensity-city");
+    fifth_dentensity_city.innerHTML = template;
+
+    /* task (d) append '-City' for cities with people property over 100000 value */
+    const big_populated_cities = document.querySelector(".big-populated-cities");
+    big_populated_cities.innerHTML = cities
+        .filter(city => city.people > 100000)
+        .map(city => city.name + "-City")
+        .join(", ");
+
+    /* task (e) more cities with population over 80000 or with under 80000 */
+    let over_populated_cities = cities
+        .filter(city => city.people > 80000)
+        .map(city => city.name + " " + city.people)
+        .length;
+
+    let under_populated_cities = cities
+        .filter(city => city.people < 80000)
+        .map(city => city.name + " " + city.people)
+        .length;
+    
+    template = `
+        Miasta powyżej 80000 mieszkańców: `
+        + over_populated_cities + `<br>
+        Miasta poniżej 80000 mieszkańców: `
+        + under_populated_cities + `<br>`;
+
+    template += over_populated_cities > under_populated_cities ?
+        `Więcej jest miast powyżej 80000 mieszkańców`
+        :
+        `Więcej jest miast poniżej 80000 mieszkańców`;
+
+    const over_or_under_populated_cities = document.querySelector(".over-or-under-populated-cities");
+    over_or_under_populated_cities.innerHTML = template;
+
+    /* task (f) give me average area of cities with first 'P' letter of township */
+    let area_sum = cities
+        .filter(city => city.township[0] === 'P')
+        .map(city => city.area)
+        .reduce( (area_a, area_b) => area_a + area_b, 0 );
+
+    let requested_township_size = cities
+        .filter(city => city.township[0] === 'P')
+        .length;
+
+    template = area_sum / requested_township_size;
+
+    const requested_township_average_area = document.querySelector(".requested-township-average-area");
+    requested_township_average_area.innerHTML = template;
 
     /* all cities */
-    template = "";
-    cities.forEach(city => {
-        template += `${city.name}, `
-    });
     const all_cities = document.querySelector(".all-cities");
-    all_cities.innerHTML = template;
+    all_cities.innerHTML = cities
+        .map(city => city.name)
+        .join(", ");
 }
 
 window.addEventListener("DOMContentLoaded", () => renderCities());
--- lab3/zadanie08/script.js
+++ lab3/zadanie08/script.js
@@ -3,10 +3,10 @@ document.addEventListener("keydown", e => {
     if(baloon.getAttribute("exploded")){
         return;
     }
+    let fontSizeText = window.getComputedStyle(baloon).getPropertyValue("font-size");
+    let fontSize = fontSizeText.slice(0, -2);
     if(e.code === "ArrowUp"){
-        let fontSizeText = window.getComputedStyle(baloon).getPropertyValue('font-size');
-        let fontSize = fontSizeText.slice(0, -2);
-        if(fontSize >= 700){
+        if(fontSize >= 300){
             baloon.innerText = String.fromCodePoint(0x1F4A5);
             baloon.setAttribute("exploded", true);
             return;
@@ -14,10 +14,32 @@ document.addEventListener("keydown", e => {
         baloon.style.fontSize = Math.round(fontSize * 1.1) + "px";
     }
     else if (e.code === "ArrowDown"){
-        let fontSizeText = window.getComputedStyle(baloon).getPropertyValue('font-size');
-        let fontSize = fontSizeText.slice(0, -2);
         if(fontSize <= 50) return;
         baloon.style.fontSize = Math.round(fontSize / 1.1) + "px";
     }
 
+});
+
+const contextMenu = document.querySelector(".context-menu");
+const baloon = document.getElementById("baloon");
+
+function showContextMenu (show = true) {
+    let baloonSizeText = window.getComputedStyle(baloon).getPropertyValue("font-size");
+    let baloonSize = baloonSizeText.slice(0, -2);
+    //console.log(baloon_size);
+    contextMenu.style.display = show ? "block" : "none";
+    contextMenu.textContent = "Rozmiar balonika: " + baloonSizeText;
+    setTimeout(() => {
+        document.querySelector(".context-menu").style.setProperty("display", "none");
+    }, 1500);
+}
+
+baloon.addEventListener("contextmenu", e => {
+    if(baloon.getAttribute("exploded") || !e.ctrlKey) return;
+    e.preventDefault();
+    showContextMenu(true);
+});
+
+window.addEventListener("click", e => {
+    showContextMenu(false);
 });
